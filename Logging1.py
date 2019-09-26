509bcd0654da7d2cfd8346f986bf8bcac4f2e69c
Avani1992 <52242393+Avani1992@users.noreply.github.com>
2019-09-26 15:13:58 +0530

    Update Logging1.py

--- Logging1.py
+++ Logging1.py
@@ -3,7 +3,7 @@ import logging
 class Logging1:
 
 
-        logging.basicConfig(filename="Logging.log", format='%(asctime)s %(filename)s  %(levelname)s  %(message)s', filemode="a")
+        logging.basicConfig(filename="Logging.log", format='%(asctime)s %(filename)s  %(levelname)s  %(message)s', filemode="w")
         logging.NullHandler
 
 
